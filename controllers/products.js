2f5cf6c4101779feb621f8437ca66e3e9bdb0191
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-10 20:47:20 +0530

    Add Search by name regex

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,27 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  // console.log(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
