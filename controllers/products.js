df84b72e45e03e8148a4629465999995b69d7831
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-11 13:30:39 +0530

    Add pagination

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query);
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -28,6 +28,17 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort('createdAt');
   }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
